80a70701aca451bb21480fb922d5dade1a8ae899
Rexton Kemp <rexkemp@iu.edu>
2025-02-18 11:20:39 -0500

    added week6

---
+++ week6/js/external.js
@@ -0,0 +1,17 @@
+console.log("what are we doin")
+
+const birthYear = 2004;
+
+let myName = "rex"
+
+console.log(myName, "was born", birthYear)
+
+myName = "rexton"
+
+console.warn(myName, "is cool")
+
+console.log("myName is",myName.length, "characters long")
+
+console.log("myName includes rex",myName.toUpperCase().includes("REX"))
+
+console.log("myName has lowercase:",myName !== myName.toUpperCase)

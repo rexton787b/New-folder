87b35498e2ae852445296440459c42fad6d68dcc
Rexton Kemp <rexkemp@iu.edu>
2025-04-03 11:36:51 -0400

    week12

--- week12/js/home.js
+++ week12/js/home.js
@@ -4,6 +4,7 @@ import {
     getFirestore,
     collection,
     getDocs,
+    addDoc,
 } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
 const db = getFirestore(app);
@@ -11,6 +12,8 @@ const db = getFirestore(app);
 const hadCollection = collection(db,"hopesdreams");
 
 const hadRef = document.querySelector("#had");
+const dreamFormRef = document.querySelector("#new-dream");
+const dreamTextRef = document.querySelector("#dream-text");
 
 async function getHad() {
 const hadDocs = await getDocs(hadCollection);
@@ -24,5 +27,20 @@ for(let i=0; i < hadDocs.docs.length; i++){
     hadRef.innerHTML += "<li>" + hadData.text + "</li>";
 }
 }
+
+async function addNewDreams(e) {
+    e.preventDefault();
+
+    const dreamTextValue = dreamTextRef.value;
+
+    const newDream = await addDoc(dreamsCollection, {text: dreamTextValue});
+
+    console.log(newDream);
+
+    getDreams();
+
+    dreamFormRef.reset();
+}
+dreamFormRef.onsubmit = addNewDreams
 
 getHad()
